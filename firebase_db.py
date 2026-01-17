c0e1edfd00784266fec6304acacaea4bba261795
kanhadhiwar <kanhadhiwar28@gmail.com>
2026-01-17 15:12:00 +0530

    Update firebase_db.py

--- firebase_db.py
+++ firebase_db.py
@@ -1,10 +1,13 @@
 import firebase_admin
 from firebase_admin import credentials, db
 
+# 1. Service account key (same folder me rakhi ho)
 cred = credentials.Certificate("firebase_key.json")
 
+# 2. Firebase initialize with databaseURL
 firebase_admin.initialize_app(cred, {
-    "databaseURL": "https://YOUR_PROJECT_ID-default-rtdb.asia-southeast1.firebasedatabase.app"
+    "databaseURL": "https://html-e256d-default-rtdb.asia-southeast1.firebasedatabase.app"
 })
 
+# 3. Reference (HTML pages data)
 html_ref = db.reference("html_pages")
